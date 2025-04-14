cc33c2459165a9e93be6b72dd0a976aeaa2f7844
Zachary Distant <zacdistant@gmail.com>
2025-04-14 10:02:22 +0100

    &#44; added to replace this , for netlify deployment

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -55,12 +55,12 @@ const Page = () => {
             className="presentation-text mt-[var(--top-internal-margin)] w-full md:w-3/4 opacity-full transform-none md:text-[1.5rem] font-normal"
             variants={textRevealMotion(0.3)}
           >
-            Hi&#44; I	&apos;m Zac — a Front-end Web Developer.
-            I&apos;m passionate about building engaging&#44; responsive&#44; and visually-driven digital experiences. I specialize in HTML&#44; CSS, JavaScript&#44; React&#44; and TailwindCSS&#44; and I love bringing interfaces to life with clean animations and thoughtful interactions.
+            Hi&#44; I&apos;m Zac — a Front-end Web Developer.
+            I&apos;m passionate about building engaging&#44; responsive&#44; and visually-driven digital experiences. I specialize in HTML&#44; CSS&#44; JavaScript&#44; React&#44; and TailwindCSS&#44; and I love bringing interfaces to life with clean animations and thoughtful interactions.
             <br>
             </br>
             <br />
-            I&apos;m constantly exploring new technologies and best practices to push my skills forward and create smooth, user-focused designs. Outside of coding&#44; you'll find me at the gym, spending time with my family&#44; or making the most of every opportunity that comes my way.
+            I&apos;m constantly exploring new technologies and best practices to push my skills forward and create smooth&#44; user-focused designs. Outside of coding&#44; you'll find me at the gym&#44; spending time with my family&#44; or making the most of every opportunity that comes my way.
             <br />
             <br />
             <span className='md:text-[1.2rem]'> CV Available on Request</span>
